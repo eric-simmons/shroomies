82b939d1051ea34b54e3e0761f5a8cfa03906811
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-05 21:06:16 -0600

    connection

---
+++ config/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS shroomies;
+CREATE DATABASE shroomies;
---
+++ models/mushroom.js
@@ -0,0 +1,9 @@
+//name
+//description non-scientific DISCLAIMER
+//location
+//timestamp
+//edible? poisonous?
+//photo filename
+//comments belong to both mushroom and the user tables and comment table
+//mushrooms belong to user
+
