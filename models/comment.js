32a7f8834f76c7bb369280cdc7a9fcf132788165
schrolan <schrolan000@gmail.com>
2022-12-07 19:22:50 -0600

    Added the js for the comment model

--- models/comment.js
+++ models/comment.js
@@ -1,5 +1,39 @@
-//id
-//content 
-//user references user
-//belong to user
-//timestamp
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey:true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+          date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+          },
+          user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+          },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelName: 'comment',
+    }
+);
+
+model.exports = Comment;
