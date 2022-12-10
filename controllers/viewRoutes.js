2178c94f344ffee97a329327cdafa50ade36fe6e
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-10 13:35:55 -0600

    commentCount

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,34 +1,43 @@
 const router = require('express').Router()
-const { Post, User } = require('../models')
+const { Post, User, Comment } = require('../models')
 
 //serve up posts on home page
 router.get('/', async (req, res) => {
     try {
-        let posts = await Post.findAll()
+        let posts = await Post.findAll({
+            include: [{ model: Comment }]
+        })
+
+        posts = posts.map(post => {
+            const postData = post.get({ plain: true })
+            return {
+                ...postData,
+                commentCount : postData.comments.length
+            }
     
-        posts = posts.map(post => post.get({ plain: true }))
-    console.log(posts)
-        res.render('home', { 
+        })
+        console.log(posts)
+        res.render('home', {
             posts,
             logged_in: req.session.logged_in
-         })
-    } catch(err) {
+        })
+    } catch (err) {
         res.status(500).json(err)
     }
 })
 //serve up post by id
 router.get('/post/:id', async (req, res) => {
     try {
-    let post = await Post.findByPk(req.params.id)
-    post = post.get({ plain: true })
+        let post = await Post.findByPk(req.params.id)
+        post = post.get({ plain: true })
 
-    res.render('post', { 
-        post,
-        logged_in: req.session.logged_in
-     })
-} catch(err) {
-    res.status(500).json(err)
-}
+        res.render('post', {
+            post,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 router.get('/login', (req, res) => {
