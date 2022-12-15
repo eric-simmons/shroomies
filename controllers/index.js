468752d23e68baee941a6bc5002642f78666de0e
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-12-14 19:52:06 -0600

    changes?

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,6 @@ const apiRoutes = require('./api');
 const viewRoutes = require('./viewRoutes')
 
 router.use('/api', apiRoutes);
-router.use(viewRoutes)
+router.use('/', viewRoutes)
 
 module.exports = router;
